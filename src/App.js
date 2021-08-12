ac400c03e89ce20b5ac71a822e056d986dd4c73d
s-haythem <haythem.smirani@gmail.com>
2021-08-12 21:52:07 +0100

    c

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {Component} from "react";
 import Profile from "./Profile";
 import "./App.css";
 
-class App extends React.Component {
+class App extends Component {
   constructor() {
     super();
     this.state = {
